bc0ced32fc0a8dbc0c46949354ae7feebb9349ba
Ramill <ramill_ibragimov@icloud.com>
2023-09-07 08:50:30 +0300

    Initial Commit

---
+++ EasyScreenshot/EasyScreenshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyScreenshotApp.swift
+//  EasyScreenshot
+//
+//  Created by Ramill Ibragimov on 07.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct EasyScreenshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
